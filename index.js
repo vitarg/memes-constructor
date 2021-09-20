c6896f34c382188af85405f144fa5087a22c4ed3
vitarg <vitrigovm@gmail.com>
2021-09-20 21:33:00 +0300

    добавил nodemon

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(require('./routes/index'))
 
 mongoose.connect(process.env.MONGO).then(() => {
   app.listen(() => console.log("Server has been started..."));
