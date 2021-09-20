65c1d7377d763285e6c6bb2759f2f706d541eb41
vitarg <vitrigovm@gmail.com>
2021-09-20 21:03:25 +0300

    мелкий фикс

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
