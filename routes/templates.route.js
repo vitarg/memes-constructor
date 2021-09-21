d39d5b9a00ec1d259003da81ef18fe28fb663b31
vitarg <vitrigovm@gmail.com>
2021-09-21 15:19:52 +0300

    добавил роуты для Template

---
+++ routes/templates.route.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const templatesController = require('../controllers/templates.controller')
+
+const router = Router();
+
+router.post('/templates', templatesController.addTemplate)
+router.get('/templates', templatesController.getAllTemplates)
+router.get('/templates', templatesController.getTemplateById)
+
+module.exports = router;
