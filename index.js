ee0cbeb0bd92af210c343687332d9d423d9b2b03
Adam <bisliev_adam@mail.ru>
2021-09-21 22:34:11 +0300

    Почти закончил регистрацию на бэке

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 app.use(express.static("public"));
 app.use(fileUpload());
 app.use(require("./routes/index"));
 
 mongoose.connect(process.env.MONGO).then(() => {
-
   app.listen(process.env.PORT, () => console.log("Server has been started..."));
-
 
   console.log("Connected with MongoDB");
 });
---
+++ models/Token.model.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require("mongoose");
+
+const tokenSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  refreshToken: { type: String, required: true },
+});
+
+module.exports = model("Token", tokenSchema);
