b009d52a970a8d13b26dabd11ee92e35fddcb734
Akhmadov <akhmadov.sw@gmail.com>
2021-09-21 16:35:30 +0300

    Сделал контроллер для удаления мема, также применил преттиер

--- controllers/memes.controller.js
+++ controllers/memes.controller.js
@@ -1,12 +1,12 @@
-const Meme = require('../models/Meme.model');
+const Meme = require("../models/Meme.model");
 
 module.exports.memesController = {
   getAllMemes: async (req, res) => {
     try {
-      const allMemes = await Meme.find({})
-      res.json(allMemes)
+      const allMemes = await Meme.find({});
+      res.json(allMemes);
     } catch (e) {
-      res.status(401).json({error: e.toString()})
+      res.status(401).json({ error: e.toString() });
     }
   },
   addMeme: async (req, res) => {
@@ -16,11 +16,19 @@ module.exports.memesController = {
         author: req.body.author, // возьмем из токена
         likes: [],
         tags: [],
-        templateId: req.body.templateId // потом возьмем Id с помощью req.params.templateId
-      })
-      res.json(meme)
+        templateId: req.body.templateId, // потом возьмем Id с помощью req.params.templateId
+      });
+      res.json(meme);
     } catch (e) {
-      res.status(401).json({error: e.toString()})
+      res.status(401).json({ error: e.toString() });
     }
-  }
-}
+  },
+  removeMeme: async (req, res) => {
+    try {
+      await Meme.findByIdAndRemove(req.params.memeId);
+      res.json("Мем удален");
+    } catch (e) {
+      res.status(401).json({ error: e.toString() });
+    }
+  },
+};
