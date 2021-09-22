7544cef0fe5f78f08e4ef34b5b494e656fdcd99d
Adam <bisliev_adam@mail.ru>
2021-09-22 14:13:25 +0300

    Сделал бэк

--- models/Token.model.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const tokenSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  refreshToken: { type: String, required: true },
-});
-
-module.exports = model("Token", tokenSchema);
