a3548a6487a91844f9811a9c3c3824c5cfe72916
vitarg <vitrigovm@gmail.com>
2021-09-22 17:15:56 +0300

    реализовал file-upload для шаблона

--- controllers/templates.controller.js
+++ controllers/templates.controller.js
@@ -1,12 +1,24 @@
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 const Template = require("../models/Template.model");
 
 module.exports.templatesController = {
   addTemplate: async (req, res) => {
     try {
-      const { img, tags, user } = req.body;
+      const imgFile = req.files.image;
+      const { tags, user } = req.body;
+
+      const fileName = `${uuidv4()}${path.extname(imgFile.name)}`;
+
+      await imgFile.mv(
+        path.resolve(__dirname, `../public/img/${fileName}`),
+        (e) => {
+          if (e) throw e;
+        }
+      );
 
       await Template.create({
-        img,
+        img: `img/${fileName}`,
         tags,
         user,
       });
