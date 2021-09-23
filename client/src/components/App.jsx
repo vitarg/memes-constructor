36591fbfa343c706f38d54b5c16ded874b41a7a2
vitarg <vitrigovm@gmail.com>
2021-09-23 13:23:56 +0300

    исправил некоторые моменты, создал новый компонент

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import MainPage from "./pages/main/MainPage";
 import CreateMemePage from "./pages/create-meme/CreateMemePage";
+import { Button } from "@material-ui/core";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div style={{ backgroundColor: "green" }}>Шапка сайта</div>
+      <div style={{ backgroundColor: "green" }}>
+        <Button component={Link} to={"/"}>
+          На главную
+        </Button>
+      </div>
 
       <Switch>
         <Route exact path="/">
--- client/src/components/pages/main/navbar/CreateMeme.jsx
+++ client/src/components/pages/main/navbar/CreateMeme.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { Button, Link } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const CreateMeme = () => {
   return (
-    <Button variant="contained" component={Link} to='/create-meme' color={"primary"}>
+    <Button
+      variant="contained"
+      component={Link}
+      to="/create-meme"
+      color={"primary"}
+    >
       Создать Мем
     </Button>
   );
