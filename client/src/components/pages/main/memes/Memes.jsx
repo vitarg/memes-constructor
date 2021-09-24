9ee289950cf38d2f1f6acfb6d97f6432ffeb221c
Adam <bisliev_adam@mail.ru>
2021-09-24 17:23:42 +0300

    Реализовал поиск

--- client/src/components/pages/main/memes/Memes.jsx
+++ client/src/components/pages/main/memes/Memes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import {
   Box,
@@ -8,13 +8,40 @@ import {
   CardMedia,
   Grid,
 } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
 
 const Memes = () => {
   const memes = useSelector((state) => state.memes.memes);
+
+  const [search, setSearch] = useState("");
+
+  const data = memes.filter((item) => {
+    if (item.tags.length > 0) {
+      for (let i = 0; i < item.tags.length; i++) {
+        if (
+          item.tags[i] &&
+          item.tags[i].toLowerCase().includes(search.toLowerCase())
+        ) {
+          return item.tags[i].toLowerCase().includes(search.toLowerCase());
+        }
+      }
+    }
+  });
+
   return (
-    <Box sx={{ flexGrow: 1 }} style={{marginTop: 30}}>
+    <Box sx={{ flexGrow: 1 }} style={{ marginTop: 30 }}>
+      <div>
+        <TextField
+          id="outlined-search"
+          label="Search field"
+          type="search"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+      </div>
+
       <Grid container spacing={3}>
-        {memes.map((item) => {
+        {data.map((item) => {
           return (
             <Grid item xs={4}>
               <Card sx={{ maxWidth: 345 }}>
@@ -25,8 +52,12 @@ const Memes = () => {
                   image={item.img}
                 />
                 <CardActions>
-                  <Button variant="contained" color={'primary'}>Сделать мем</Button>
-                  <Button variant="contained" color={'secondary'}>Сохранить</Button>
+                  <Button variant="contained" color={"primary"}>
+                    Сделать мем
+                  </Button>
+                  <Button variant="contained" color={"secondary"}>
+                    Сохранить
+                  </Button>
                 </CardActions>
               </Card>
             </Grid>
