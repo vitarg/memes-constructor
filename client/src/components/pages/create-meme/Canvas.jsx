e6ad988e96bd11c0b0a0b7389e380a8546de7b95
vitarg <vitrigovm@gmail.com>
2021-09-28 20:47:01 +0300

    добавил библиотеку tui image editor и сделал начальные настройки

--- client/src/components/pages/create-meme/Canvas.jsx
+++ client/src/components/pages/create-meme/Canvas.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import "tui-image-editor/dist/tui-image-editor.css";
+import ImageEditor from "@toast-ui/react-image-editor";
 
 const useStyles = makeStyles({
   templateWrapper: {
@@ -13,9 +14,51 @@ const useStyles = makeStyles({
   },
 });
 
+const myTheme = {
+  // Theme object to extends default dark theme.
+};
+
+const locale_ru_RU = {
+  Crop: "Обрезать",
+  "Delete-all": "Удалить всё",
+  Text: "Текст",
+  Bold: "Жирный",
+  Italic: "Курсив",
+  Underline: "Подчеркнутый",
+  Load: "Загрузить",
+  Download: "Скачать",
+  Color: "Цвет",
+  "Text size": "Размер шрифта",
+};
+
 const Canvas = () => {
   const classes = useStyles();
-  return <Box className={classes.templateWrapper}>Выберите шаблон</Box>;
+  return (
+    <ImageEditor
+      includeUI={{
+        loadImage: {
+          path: img,
+          name: "SampleImage",
+        },
+        locale: locale_ru_RU,
+        theme: myTheme,
+        menu: ["text", "crop"],
+        initMenu: "filter",
+        uiSize: {
+          width: "1000px",
+          height: "700px",
+        },
+        menuBarPosition: "right",
+      }}
+      cssMaxHeight={500}
+      cssMaxWidth={700}
+      selectionStyle={{
+        cornerSize: 20,
+        rotatingPointOffset: 70,
+      }}
+      usageStatistics={true}
+    />
+  );
 };
 
 export default Canvas;
--- client/src/components/pages/create-meme/Create.jsx
+++ client/src/components/pages/create-meme/Create.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import { Button, Grid, TextField } from "@material-ui/core";
+import { Grid} from "@material-ui/core";
 import Canvas from "./Canvas";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  form: {
-    display: "flex",
-    flexDirection: "column",
-    width: "80%",
-    height: 400
-  },
   container: {
     marginTop: 40,
-  },
-  input: {
-    marginTop: 20,
-    background: "#fff",
-  },
-  generateBtn: {
-    marginTop: 'auto'
-  },
-  left: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
   },
 });
 
@@ -31,30 +15,7 @@ const Create = () => {
   const classes = useStyles();
   return (
     <Grid container className={classes.container}>
-      <Grid item xs={6} className={classes.left}>
-        <Canvas />
-      </Grid>
-      <Grid item xs={6}>
-        <form className={classes.form}>
-          <TextField
-            label={"Текст 1"}
-            variant={"outlined"}
-            className={classes.input}
-          />
-          <TextField
-            label={"Текст 2"}
-            variant={"outlined"}
-            className={classes.input}
-          />
-          <Button
-            color={"primary"}
-            variant={"contained"}
-            className={classes.generateBtn}
-          >
-            Сгенерировать
-          </Button>
-        </form>
-      </Grid>
+      <Canvas />
     </Grid>
   );
 };
