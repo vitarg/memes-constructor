3859097a4efed39bf779a5caffd9bfebc7a51f4a
Mansur <vitrigovm@gmail.com>
2022-02-09 15:49:40 +0300

    внес некоторые правки

--- routes/memes.route.js
+++ routes/memes.route.js
@@ -8,7 +8,7 @@ router.get("/memes", memesController.getAllMemes);
 router.get("/memes/rnd", memesController.randomMeme);
 router.get("/memes/:id", memesController.getByAuthor);
 router.post("/memes/", authMiddleware, memesController.addMeme);
-router.delete("/memes/:memeId", memesController.removeMeme);
+router.delete("/memes/:memeId", memesController.removeMemeById);
 router.post("/memes/likes/:id", authMiddleware, memesController.patchMeme);
 
 module.exports = router;
